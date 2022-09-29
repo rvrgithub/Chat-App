efc4aca5b90a7eaa4f0adcc9fbb27b4f684f30dd
radhika <rvramayra1998@gmail.com>
2022-09-29 19:44:20 +0530

    update user module

--- Backend/src/modules/user.module.js
+++ Backend/src/modules/user.module.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
+    firstName :{type:String, required:true},
+    lastName:{type:String, required:true,},
     email: {type:String, required:true, unique:true},
-    password:{type:String ,required:true}
+    password:{type:String ,required:true,minlength:8}
 },
 {
 timestamps:true,
